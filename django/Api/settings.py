21a641df4b8b7566aade4c513dc7a90aab3b6e79
Tanner Olsen <tannerolsen7@gmail.com>
2021-02-19 17:55:16 -0700

    Revert "Take out secret key"
    This reverts commit 00280844aa18331d8da841dbc321490314ba6709.
    get everything working again

--- django/Api/settings.py
+++ django/Api/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = '=_eplq%!rl1hrtxkdw8u^)a5fq_@@*l3-obj-o1i7r7@_h3g&('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
